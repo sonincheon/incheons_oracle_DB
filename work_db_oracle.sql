e27163cd0845e0ada0f61cb0de3cd42cbd57f499
incheon94 <sonincheon94@naver.com>
2023-09-14 18:05:24 +0900

    9.14 데이터베이스

--- work_db_oracle.sql
+++ work_db_oracle.sql
@@ -191,29 +191,29 @@ WHERE ENAME LIKE '%S';
 
 SELECT EMPNO,ENAME,JOB,SAL,DEPTNO
 FROM EMP
-WHERE JOB='SALESMAN' AND
- DEPTNO = 30;
+WHERE JOB='SALESMAN' 
+AND DEPTNO = 30;
 
 -- 3. 20, 30번 부서에 근무하는 사원 중 급여 2000 초과인 사원인 사원번호,이름,급여,부서 번호출력
 
 SELECT EMPNO,ENAME,JOB,SAL,DEPTNO
 FROM EMP
-WHERE SAL >2000 AND 
-DEPTNO IN (20,30);
+WHERE SAL >2000 
+AND DEPTNO IN (20,30);
 
 --4. BETWEEN 연산자 사용하지 않고 급여가 2000이상 3000이하 데이터 출력
 
 SELECT *
 FROM EMP
-WHERE SAL >= 2000 AND
- SAL <= 3000;
+WHERE SAL >= 2000
+AND SAL <= 3000;
 
 -- 5. 사원 이름에 E가 포함되어 있는 30번 부서의 사원중 급여가 1000 ~ 2000사이가 아닌 사원이름
 SELECT EMPNO,ENAME,JOB,SAL,DEPTNO
 FROM EMP
-WHERE DEPTNO =30 AND 
-ENAME LIKE'%E%' AND 
-SAL NOT BETWEEN 1000 AND 2000 ;
+WHERE DEPTNO =30 
+AND ENAME LIKE'%E%' 
+AND SAL NOT BETWEEN 1000 AND 2000 ;
 
 -- 6. 추가수당이 존재하지않고 상급자가 있고  직책이 MANAGER, CLERK인 사원에서
 -- 사원 이름이 두번쨰 글자가 L이 아닌 사원의 정보를 출력함
@@ -224,10 +224,94 @@ AND MGR IS NOT NULL
 AND JOB IN ('CLERK','MANAGER') 
 AND ENAME NOT LIKE '_L%' ;
 
+-- 1. **EMP테이블에서 COMM 의 값이 NULL이 아닌 정보 조회**
+SELECT *
+FROM EMP
+WHERE COMM IS NOT NULL;
+
+-- 2. **EMP테이블에서 커미션을 받지 못하는 직원 조회**
+SELECT *
+FROM EMP
+WHERE COMM IS NULL OR COMM = 0;
+
+-- 3. **EMP테이블에서 관리자가 없는 직원 정보 조회**
+SELECT *
+FROM EMP
+WHERE MGR IS NULL;
 
 
+-- 4. **EMP테이블에서 급여를 많이 받는 직원 순으로 조회**
+SELECT *
+FROM EMP
+ORDER BY SAL DESC;
+-- 5. **EMP테이블에서 급여가 같을 경우 커미션을 내림차순 정렬 조회
+**
+SELECT *
+FROM EMP
+ORDER BY SAL DESC, COMM DESC;
 
+-- 6. **EMP테이블에서 사원번호, 사원명,직급, 입사일 조회 (단, 입사일을 오름차순 정렬 처리)**
+SELECT EMPNO,ENAME,JOB,HIREDATE
+FROM EMP
+ORDER BY HIREDATE ASC;
+-- 7. **EMP테이블에서 사원번호, 사원명 조회 (사원번호 기준 내림차순 정렬)**
+SELECT EMPNO,ENAME
+FROM EMP
+ORDER BY EMPNO DESC;
 
+-- 8. **EMP테이블에서 사번, 입사일, 사원명, 급여 조회  (부서번호가 빠른 순으로, 같은 부서번호일 때는 최근 입사일 순으로 처리)**
+SELECT EMPNO,HIREDATE,ENAME,SAL
+FROM EMP
+ORDER BY DEPTNO ASC, HIREDATE DESC;
+
+-- 9.오늘 날짜에 대한 정보 조회
+SELECT SYSDATE
+FROM DUAL;
+
+-- ------------------------------------------------------------
+-- 함수 DELETE
+--오라클에서 함수는 내장 함수 사용자 정의 함수로 나누어
+-- 내장 함수는 단일행 함수와 다중행(집계) 함수로 나누어짐
+-- DUAL 테이블: 오라클의 SYS계정에서 제공하는 테이블로 함수나 
+-- 계산식에서 테이블 참조없이 실행해 보기 위해 제공됨 (DUMMY 테이블)
+
+-- ABS : 절대값을 구하는 함수 
+SELECT -10 , ABS(-10) 
+FROM DUAL; 
+
+--ROUND : 반올림한 결과를 반환하는 함수 : ROUND(숫자, 반올림 위치 ), 위치는 음수값도 줄수 있음
+SELECT ROUND(1234.5678) AS ROUND, 
+    ROUND(1234.5678, 0) AS ROUND_0,
+    ROUND (1234.5678, 1) AS ROUND_1,
+    ROUND(1234.5678, 2) AS ROUND_2,
+    ROUND(1234.5678, -1) AS ROUND_MINUS1,
+    ROUND(1234.5678, -2) AS ROUND_MINUS2
+FROM DUAL;
+
+-- TRUNC : 버림한 결과를 반환하는 함수 
+SELECT TRUNC(1234.5678) AS TRUNC, 
+    TRUNC(1234.5678, 0) AS TRUNC_0,
+    TRUNC (1234.5678, 1) AS TRUNC_1,
+    TRUNC(1234.5678, 2) AS TRUNC_2,
+    TRUNC(1234.5678, -1) AS TRUNC_MINUS1,
+    TRUNC(1234.5678, -2) AS TRUNC_MINUS2
+FROM DUAL;
+
+-- MOD : 나누기한 나머지만 출력 
+SELECT MOD(21, 5) 
+FROM DUAL;
+
+-- CEIL : 소수점 이하를 무조건 올림 
+SELECT CEIL(12.00001)
+FROM DUAL;
+
+--FLOOR : 소수점 이하를 무조건 내림 
+SELECT FLOOR(12.00001)
+FROM DUAL;
+
+--POER : 정수 A를 정수 B만큼 제곱하는 함수 (3 ,4)
+SELECT POWER(3,4)
+FROM DUAL;
 
 
 
